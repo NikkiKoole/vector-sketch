a8d8ac2ad8398f5c04f7ecacb73f58b4e1e000de
Nikki Koole <nikki@floorplanner.com>
2021-12-02 11:47:53 +0100

    organizing the code a bit better, its a dlist after all

--- experiments/scrolling-world-ecs/src/systems/StackSystem.lua
+++ experiments/scrolling-world-ecs/src/systems/StackSystem.lua
@@ -1,22 +1,9 @@
 --[[
-
-stackable:  means this is a normal item that is allowed to be placed in a stack
-inStack  : means this thing is in a stack, inStack has a prev & next link (double linked list)
+stackable :  means this is a normal item that is allowed to be placed in a stack
+inStack   :  means this thing is in a stack, inStack has a prev & next link (double linked list)
 ]]--
 local StackSystem = Concord.system({pool = {'stackable'}})
 
-
-
-
-
---function StackSystem:update(dt)
---end
---function StackSystem:itemDrag( c, l, x, y, invx, invy)
--- if (c.entity and c.entity.vanillaDraggable and c.pressed) then
---    c.transforms.l[1] = c.transforms.l[1] + (invx - c.pressed.dx)
---    c.transforms.l[2] = c.transforms.l[2] + (invy - c.pressed.dy)
--- end
---end
 
 local MAX_DISTANCE_TO_CONNECT = 20
 
@@ -24,7 +11,7 @@ function StackSystem:itemThrow(target, dxn, dyn, speed)
    -- an item has been released and might want to end up in a stack.
    
    if target.entity.stackable then
-      local foundOneToConnectMyselfTo = nil
+      local connectTo = nil
 
       -- figure out if anything is near i can connect myself to
       
@@ -42,6 +29,8 @@ function StackSystem:itemThrow(target, dxn, dyn, speed)
 	       local tag = checkAgainst[j].metaTags[k]
 
 	       if (tag.name == 'connector' and checkAgainst[j] ~= target) then
+		  -- todo you cant connect to items that alread have an inStack & next
+		  -- those are taken already
 		  local pos = tag.points[1] -- there is just one point in this collection
 		  local kx, ky = checkAgainst[j].transforms._g:transformPoint(pos[1], pos[2])
 		  local camData = createCamData(checkAgainst[j], pdata)
@@ -58,17 +47,17 @@ function StackSystem:itemThrow(target, dxn, dyn, speed)
 
 	 --print('the nearest connection possibile is ', nearest.distance)
 	 if (nearest.distance <= MAX_DISTANCE_TO_CONNECT) then
-	    foundOneToConnectMyselfTo = nearest.elem
+	    connectTo = nearest.elem
 	 end
 	 
 	
 	 -- found one to connect myself to
-	 if foundOneToConnectMyselfTo then
+	 if connectTo then
 	    target.entity:remove('inMotion')
 
 	    local changeDepth = true
 	    if changeDepth then
-	       target.depth = foundOneToConnectMyselfTo.depth + 0.01
+	       target.depth = connectTo.depth + 0.01
 	       local layer, pdata = retrieveLayerAndParallax(target.entity.layer.index)
 	       sortOnDepth(layer.children)
 	    end
@@ -78,40 +67,67 @@ function StackSystem:itemThrow(target, dxn, dyn, speed)
 	 -- if not, we need to create the newly made stack now
 
 	 -- todo when do we remove the inStack component ?
+	 -- todo there are still bugs here
+	 -- best foound by playing the 3 stack game with the piramids
+	 
+	 if connectTo then
+	    -- the question that isnt asked here is:
+	    -- am i (target) already inStack ?
+	    
+	    if connectTo.entity.inStack then
+	       connectTo.entity.inStack.next = target
 
-	 if foundOneToConnectMyselfTo then
-	    if foundOneToConnectMyselfTo.entity.inStack then
-	       foundOneToConnectMyselfTo.entity.inStack.next = target
-	       target.entity:give('inStack', foundOneToConnectMyselfTo, nil)
+	       if target.entity.inStack then
+
+		  -- my old previous (if there needs to next to nothing)
+		  local prev = target.entity.inStack.prev
+		  if prev then
+		     prev.entity.inStack.next = nil
+		  end
+		  
+		  target.entity.inStack.prev = connectTo
+	       else
+		  
+		  target.entity:give('inStack', connectTo, nil)
+	       end
 	    else
-	       foundOneToConnectMyselfTo.entity:give('inStack', nil, target)
-	       target.entity:give('inStack', foundOneToConnectMyselfTo, nil)
+	       connectTo.entity:give('inStack', nil, target)
+	       if target.entity.inStack then
+		  target.entity.inStack.prev = connectTo
+	       else
+		  target.entity:give('inStack', connectTo, nil)
+	       end
 	    end
 	 end
 
-	 if not foundOneToConnectMyselfTo then
+	 if not connectTo then
 	    if target.entity.inStack then
 	       --print('this thing is in a stack, but we didnt find anything to connect too')
 	       -- atleast remove the inStack prev link from both sides
 	       local prev = target.entity.inStack.prev
 	       if prev then
 		  prev.entity.inStack.next = nil
+		  -- figure out if prev is still allowed to be inStack itself (if its prev == nil too it isnt)
 	       end
 	       target.entity.inStack.prev = nil
 	       
 	    end
 	 end
-	 
-	 
-         
-	 
-	 
+
       end
       
    end
 end
 
---function StackSystem:pressed(x,y)
---end
+-- when you connect something (stack or item) to something else (stack or item)
+function insertNodeAfter(node, after)
+end
+
+-- when you remove something (stack or item) from a stack
+function removeNode(node)
+end
+
+
+
 
 return StackSystem
