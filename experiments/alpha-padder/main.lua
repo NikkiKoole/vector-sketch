ade21396d86ec3bd6419410eeb27db4b70a06e2c
Nikki Koole <nikki@floorplanner.com>
2022-07-05 10:53:02 +0200

    dont understand but it was working as expected already

--- experiments/alpha-padder/main.lua
+++ experiments/alpha-padder/main.lua
@@ -1,5 +1,3 @@
--- https://github.com/urraka/alpha-bleeding
---tgis is a windows executa l
 
 
 function love.keypressed(key)
@@ -16,31 +14,22 @@ function doImage(url, name, data)
    local width, height = imageData:getDimensions()
    local format = imageData:getFormat( )
    local result = love.image.newImageData( width, height,format)
-
    local count = 0
-
    
    for y = 0, height -1 do
 	for x = 0, width-1 do
 	   local r, g, b, a = imageData:getPixel(x, y)
-
-           
            if a == 0 then
               for x2 = -1,1 do
                  for y2 = -1,1 do
                     if (x+x2) >=0 and (x+x2)<=width-1 then
                        if (y+y2) >=0 and (y+y2)<=height-1 then
-                          
                           local r, g, b, a = imageData:getPixel(x+x2, y+y2)
 
                           if (a>0) then
                              count = count + 1
-
                              result:setPixel(x,y,r,g,b,0)
-
-                             --print('want to take')
                           end
-                          
                        end
                     end
                  end
@@ -53,23 +42,20 @@ function doImage(url, name, data)
 	end
    end
    
-   print('work todo', count)
-   
    image = love.graphics.newImage(result, {mipmaps=true})
-   
    result:encode("png",name)
    love.system.openURL("file://"..love.filesystem.getSaveDirectory())
 end
 
 function mysplit (inputstr, sep)
-        if sep == nil then
-                sep = "%s"
-        end
-        local t={}
-        for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
-                table.insert(t, str)
-        end
-        return t
+   if sep == nil then
+      sep = "%s"
+   end
+   local t={}
+   for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+      table.insert(t, str)
+   end
+   return t
 end
 
 function love.load()
@@ -86,24 +72,16 @@ function getFiledata(filename)
 end
 
 
-function mountZip(filename, mountpoint)
-   local filedata = getFiledata(filename)
-   return love.filesystem.mount(filedata, mountpoint or 'zip')
-end
+--function mountZip(filename, mountpoint)
+--   local filedata = getFiledata(filename)
+--   return love.filesystem.mount(filedata, mountpoint or 'zip')
+--end
 
 function love.filedropped(file)
---   print(file:getFilename())
-
    local fullPath = file:getFilename()
-
    local d = getFiledata(fullPath)
-
    local splitted = mysplit(fullPath, "/")
    local name = splitted[#splitted]
---   for i=1, #splitted do
---      print(splitted[i])
---   end
-
 
    doImageFromData(d, name)
    
@@ -113,7 +91,10 @@ end
 function love.draw()
    love.graphics.clear(.5,.6,.4)
 
+   love.graphics.setColor(0,0,0)
    love.graphics.print("drop image file to fix padding issues", 300,300)
-   --love.graphics.setBlendMode("multiply", "premultiplied")
---   love.graphics.draw(image,-200,-200, 0.1,4,4)
+   love.graphics.setColor(1,1,1)
+
+   love.graphics.print("drop image file to fix padding issues", 301,301)
+
 end
