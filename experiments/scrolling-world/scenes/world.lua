d5c8c7aec460437bca800213cbfcfec84fe10b4a
Nikki Koole <nikkikoole@gmail.com>
2022-09-25 10:11:07 +0200

    ws

--- experiments/scrolling-world/scenes/world.lua
+++ experiments/scrolling-world/scenes/world.lua
@@ -3,13 +3,13 @@ local mesh = require 'lib.mesh'
 local scene = {}
 local hasBeenLoaded = false
 
-  -- local Entity     = Concord.entity
-  -- local Component  = Concord.component
-  -- local System     = Concord.system
-  -- local World      = Concord.world
+-- local Entity     = Concord.entity
+-- local Component  = Concord.component
+-- local System     = Concord.system
+-- local World      = Concord.world
 
 -- Containers
-  -- local Components  = Concord.components
+-- local Components  = Concord.components
 
 -- look at some
 -- https://www.istockphoto.com/nl/portfolio/Sashatigar?mediatype=illustration
@@ -29,44 +29,51 @@ function attachPointerCallbacks()
          SM.load('intro')
       end
       if key == 'up' then
-	 cam:translate(0, -10)
+         cam:translate(0, -10)
       end
       if key == 'down' then
-	 cam:translate(0, 10)
+         cam:translate(0, 10)
       end
-      love.keyboard.setKeyRepeat( true )
+      love.keyboard.setKeyRepeat(true)
    end
-   function love.mousepressed(x,y, button, istouch, presses)
+
+   function love.mousepressed(x, y, button, istouch, presses)
       if (mouseState.hoveredSomething) then return end
       if not istouch then
-         pointerPressed(x,y, 'mouse', parallaxLayersData)
+         pointerPressed(x, y, 'mouse', parallaxLayersData)
       end
    end
+
    function love.touchpressed(id, x, y, dx, dy, pressure)
-      pointerPressed(x,y, id, parallaxLayersData)
+      pointerPressed(x, y, id, parallaxLayersData)
    end
-   function love.mousemoved(x, y,dx,dy, istouch)
+
+   function love.mousemoved(x, y, dx, dy, istouch)
       if not istouch then
-         pointerMoved(x,y,dx,dy, 'mouse', parallaxLayersData)
+         pointerMoved(x, y, dx, dy, 'mouse', parallaxLayersData)
       end
    end
-   function love.touchmoved(id, x,y, dx, dy, pressure)
-      pointerMoved(x,y,dx,dy, id, parallaxLayersData)
+
+   function love.touchmoved(id, x, y, dx, dy, pressure)
+      pointerMoved(x, y, dx, dy, id, parallaxLayersData)
    end
-   function love.mousereleased(x,y, button, istouch)
+
+   function love.mousereleased(x, y, button, istouch)
       lastDraggedElement = nil
       if not istouch then
-         pointerReleased(x,y, 'mouse', parallaxLayersData)
+         pointerReleased(x, y, 'mouse', parallaxLayersData)
       end
    end
+
    function love.touchreleased(id, x, y, dx, dy, pressure)
-      pointerReleased(x,y, id, parallaxLayersData)
+      pointerReleased(x, y, id, parallaxLayersData)
    end
+
    function eventBus(event)
       if event == 'door-hitarea' then
-	 -- tween camera to cave opening
-	 -- go into cave
-	 SM.load("cave")
+         -- tween camera to cave opening
+         -- go into cave
+         SM.load("cave")
       end
 
 
@@ -76,7 +83,7 @@ end
 
 function scene.load()
 
-   local timeIndex = math.floor(1 + love.math.random()*24)
+   local timeIndex = math.floor(1 + love.math.random() * 24)
 
    skygradient = gradientMesh(
       "vertical",
@@ -85,12 +92,12 @@ function scene.load()
 
    if not hasBeenLoaded then
 
-         --print('world:', World)
+      --print('world:', World)
 
 
-      depthMinMax =       {min=-1.0, max=1.0}
-      foregroundFactors = { far=.8, near=1}
-      backgroundFactors = { far=.4, near=.7}
+      depthMinMax = { min = -1.0, max = 1.0 }
+      foregroundFactors = { far = .8, near = 1 }
+      backgroundFactors = { far = .4, near = .7 }
       tileSize = 100
 
       --cam = createCamera()
@@ -104,28 +111,28 @@ function scene.load()
       dynamic = generateCameraLayer('dynamic', 1)
 
       backgroundAssetBook = generateAssetBook({
-            urls= createAssetPolyUrls({'doosgroot'}),
-            index={min=-100, max= 100},
-            amountPerTile=1,
-            depth=depthMinMax,
+         urls = createAssetPolyUrls({ 'doosgroot' }),
+         index = { min = -100, max = 100 },
+         amountPerTile = 1,
+         depth = depthMinMax,
       })
       backgroundLayer = makeContainerFolder('backgroundLayer')
 
       foregroundAssetBook = generateAssetBook({
-            urls= createAssetPolyUrls(
-               { 'plant1','plant2','plant3','plant4',
-                  'plant5','plant6','plant7','plant8',
-                  'plant9','plant10','plant11','plant12',
-                 'plant13','bunnyhead', 'deurpaars', 'deurpaars'
+         urls = createAssetPolyUrls(
+            { 'plant1', 'plant2', 'plant3', 'plant4',
+               'plant5', 'plant6', 'plant7', 'plant8',
+               'plant9', 'plant10', 'plant11', 'plant12',
+               'plant13', 'bunnyhead', 'deurpaars', 'deurpaars'
             }),
-            index={min=-100, max= 100},
-            amountPerTile=5,
-            depth=depthMinMax,
+         index = { min = -100, max = 100 },
+         amountPerTile = 5,
+         depth = depthMinMax,
       })
       foregroundLayer = makeContainerFolder('foregroundLayer')
 
 
-      groundPlanes = makeGroundPlaneBook(createAssetPolyUrls({'fit1', 'fit2', 'fit3', 'fit4', 'fit5'}))
+      groundPlanes = makeGroundPlaneBook(createAssetPolyUrls({ 'fit1', 'fit2', 'fit3', 'fit4', 'fit5' }))
 
 
       --generateRandomPolysAndAddToContainer(30, foregroundFactors, foregroundLayer)
@@ -134,14 +141,14 @@ function scene.load()
       function makeObject(url, x, y, depth, allowOptimized)
          if allowOptimized == nil then allowOptimized = true end
          local read = readFileAndAddToCache(url)
-	 local doOptimized = read.optimizedBatchMesh ~= nil
+         local doOptimized = read.optimizedBatchMesh ~= nil
 
-	 local child = {
-	    folder = true,
-	    transforms = copy3(read.transforms),
-	    name = 'generated '..url,
-	    children = (allowOptimized and doOptimized) and {} or copy3(read.children)
-	 }
+         local child = {
+            folder = true,
+            transforms = copy3(read.transforms),
+            name = 'generated ' .. url,
+            children = (allowOptimized and doOptimized) and {} or copy3(read.children)
+         }
          if allowOptimized and doOptimized then
             child.url = url
          end
@@ -150,34 +157,33 @@ function scene.load()
          child.transforms.l[1] = x
          child.transforms.l[2] = y
 
-	 child.bbox = read.bbox
+         child.bbox = read.bbox
          child.metaTags = read.metaTags
-         print(inspect(child.bbox),x,y)
+         print(inspect(child.bbox), x, y)
          mesh.meshAll(child)
          return child
       end
 
       function makeWheel(thing, circumference)
          thing.wheelCircumference = circumference
-	 return thing
+         return thing
       end
-
 
       table.insert(
          foregroundLayer.children,
-         makeObject('assets/cavething.polygons.txt', 1000,0, 0)
+         makeObject('assets/cavething.polygons.txt', 1000, 0, 0)
       )
       table.insert(
          backgroundLayer.children,
-         makeWheel(makeObject('assets/wiel.polygons.txt', 1100,0, -1), 282)
+         makeWheel(makeObject('assets/wiel.polygons.txt', 1100, 0, -1), 282)
       )
       table.insert(
          foregroundLayer.children,
-         makeWheel(makeObject('assets/wiel.polygons.txt', 1100,0, 1), 282)
+         makeWheel(makeObject('assets/wiel.polygons.txt', 1100, 0, 1), 282)
       )
       table.insert(
          foregroundLayer.children,
-         makeWheel(makeObject('assets/wiel.polygons.txt', 1100,0, -1), 282)
+         makeWheel(makeObject('assets/wiel.polygons.txt', 1100, 0, -1), 282)
       )
 
       -- table.insert(
@@ -189,65 +195,64 @@ function scene.load()
       --    makeObject('assets/walterhappyfeetright_.polygons.txt', 0,0, 0)
       -- )
 
-      actors  = {}
+      actors = {}
       for i = 1, 1 do
-         walterBody =  makeObject('assets/walterbody.polygons.txt', 0,0,love.math.random(), false)
-	 walterLFoot =  makeObject('assets/walterhappyfeetleft_.polygons.txt', 0,0, 0)
-	 walterRFoot =  makeObject('assets/walterhappyfeetright_.polygons.txt', 0,0, 0)
+         walterBody = makeObject('assets/walterbody.polygons.txt', 0, 0, love.math.random(), false)
+         walterLFoot = makeObject('assets/walterhappyfeetleft_.polygons.txt', 0, 0, 0)
+         walterRFoot = makeObject('assets/walterhappyfeetright_.polygons.txt', 0, 0, 0)
 
 
          walterBody.hasDraggableChildren = true
          walterLFoot.isDraggableChild = true
          walterRFoot.isDraggableChild = true
---         walterBody.transforms.l[2]=-100
+         --         walterBody.transforms.l[2]=-100
 
-	 walterActor = Actor:create({body=walterBody, lfoot=walterLFoot, rfoot=walterRFoot})
+         walterActor = Actor:create({ body = walterBody, lfoot = walterLFoot, rfoot = walterRFoot })
 
---         walterActor.body.actorRef = walterActor
-	 table.insert(
-	    foregroundLayer.children,
-	    walterActor.body
-	 )
-	 -- table.insert(
-	 --    foregroundLayer.children,
-	 --    walterLFoot
-	 -- )
+         --         walterActor.body.actorRef = walterActor
+         table.insert(
+            foregroundLayer.children,
+            walterActor.body
+         )
+         -- table.insert(
+         --    foregroundLayer.children,
+         --    walterLFoot
+         -- )
 
-	 table.insert(actors, walterActor)
+         table.insert(actors, walterActor)
       end
 
       parentize.parentize(foregroundLayer)
       parallax.sortOnDepth(foregroundLayer.children)
       mesh.recursivelyAddOptimizedMesh(foregroundLayer)
-     
+
       parallaxLayersData = {
-	 {
-            layer=backgroundLayer,
-            p={factors=backgroundFactors, minmax=depthMinMax},
-            assets=backgroundAssetBook,
-            tileBounds={math.huge, -math.huge},
-	 },{
-            layer=foregroundLayer,
-            p={factors=foregroundFactors, minmax=depthMinMax},
-            assets=foregroundAssetBook,
-            tileBounds={math.huge, -math.huge},
-	   }
+         {
+            layer = backgroundLayer,
+            p = { factors = backgroundFactors, minmax = depthMinMax },
+            assets = backgroundAssetBook,
+            tileBounds = { math.huge, -math.huge },
+         }, {
+            layer = foregroundLayer,
+            p = { factors = foregroundFactors, minmax = depthMinMax },
+            assets = foregroundAssetBook,
+            tileBounds = { math.huge, -math.huge },
+         }
       }
 
 
 
-    
-   end
-   perspectiveContainer = preparePerspectiveContainers({'foreground', 'background'})
 
-   setCameraViewport(cam, 400,400)
+   end
+   perspectiveContainer = preparePerspectiveContainers({ 'foreground', 'background' })
+
+   setCameraViewport(cam, 400, 400)
    hasBeenLoaded = true
    attachPointerCallbacks()
    --foregroundLayer.dirty = true
 
    --print(inspect(foregroundLayer.children[1]))
 end
-
 
 function scene.update(dt)
    if love.keyboard.isDown('p') then
@@ -269,11 +274,11 @@ function scene.update(dt)
    handlePressedItemsOnStage(dt, parallaxLayersData)
 
    --for i = 1, #foregroundLayer.children do
-      --if not foregroundLayer.children[i].pressed then
-      --foregroundLayer.children[i].transforms.l[3] = foregroundLayer.children[i].transforms.l[3] + 0.01
-      --end
+   --if not foregroundLayer.children[i].pressed then
+   --foregroundLayer.children[i].transforms.l[3] = foregroundLayer.children[i].transforms.l[3] + 0.01
    --end
-   for i=1, #actors do
+   --end
+   for i = 1, #actors do
       actors[i]:update(dt)
    end
 
@@ -281,29 +286,29 @@ end
 
 function scene.draw()
 
-   love.graphics.clear(1,1,1)
-   love.graphics.setColor(1,1,1)
+   love.graphics.clear(1, 1, 1)
+   love.graphics.setColor(1, 1, 1)
 
    love.graphics.draw(skygradient, 0, 0, 0, love.graphics.getDimensions())
 
-   drawGroundPlaneWithTextures(cam, 'backgroundFar', 'backgroundNear' ,'background')
+   drawGroundPlaneWithTextures(cam, 'backgroundFar', 'backgroundNear', 'background')
    drawGroundPlaneWithTextures(cam, 'foregroundFar', 'foregroundNear', 'foreground')
 
    arrangeParallaxLayerVisibility('backgroundFar', parallaxLayersData[1])
    cam:push()
-   GLOBALS.parallax = {camera=dynamic, p=parallaxLayersData[1].p }
+   GLOBALS.parallax = { camera = dynamic, p = parallaxLayersData[1].p }
 
    render.renderThings(backgroundLayer)
    cam:pop()
 
    arrangeParallaxLayerVisibility('foregroundFar', parallaxLayersData[2])
    cam:push()
-   GLOBALS.parallax = {camera=dynamic, p=parallaxLayersData[2].p }
+   GLOBALS.parallax = { camera = dynamic, p = parallaxLayersData[2].p }
 
-   render.renderThings( foregroundLayer)
+   render.renderThings(foregroundLayer)
    cam:pop()
 
-   love.graphics.setColor(1,1,1)
+   love.graphics.setColor(1, 1, 1)
    --drawUI()
    drawDebugStrings()
    drawBBoxAroundItems(foregroundLayer, parallaxLayersData[2].p)
--- src/palettes.lua
+++ src/palettes.lua
@@ -1,89 +1,94 @@
 function hex2rgb(hex)
-    hex = hex:gsub("#","")
-    return tonumber("0x"..hex:sub(1,2)), tonumber("0x"..hex:sub(3,4)), tonumber("0x"..hex:sub(5,6))
+   hex = hex:gsub("#", "")
+   return tonumber("0x" .. hex:sub(1, 2)), tonumber("0x" .. hex:sub(3, 4)), tonumber("0x" .. hex:sub(5, 6))
 end
 
 local miffy = {
-   name='miffy',
-   colors={
-      {   rgb={48,112,47}},
-      {   rgb={27,84,154}},
-      {  rgb={250,199,0}},
-      { rgb={233,100,14}},
-      { rgb={237,76,6}},
-      { rgb={221,61,14}},
-      { rgb={34,30,30}},
-      { rgb={24,26,23}},
-      { rgb={24,26,23}},
-      { rgb={145,77,35}},
-      { rgb={114,65,11}},
-      { rgb={136,95,62}},
-      { rgb={147,142,114}},
-      {rgb={149,164,151}},
+   name = 'miffy',
+   colors = {
+      { rgb = { 48, 112, 47 } },
+      { rgb = { 27, 84, 154 } },
+      { rgb = { 250, 199, 0 } },
+      { rgb = { 233, 100, 14 } },
+      { rgb = { 237, 76, 6 } },
+      { rgb = { 221, 61, 14 } },
+      { rgb = { 34, 30, 30 } },
+      { rgb = { 24, 26, 23 } },
+      { rgb = { 24, 26, 23 } },
+      { rgb = { 145, 77, 35 } },
+      { rgb = { 114, 65, 11 } },
+      { rgb = { 136, 95, 62 } },
+      { rgb = { 147, 142, 114 } },
+      { rgb = { 149, 164, 151 } },
    }
 }
 local lego = {
-   name='lego-classic',
-   colors={
+   name = 'lego-classic',
+   colors = {
       --{name="bright red", rgb={196,40,27}},
-      { rgb={13,105,171}},
-      { rgb={245,205,47}},
-       { rgb={40,127,70}},
-     {rgb={242,243,242}},
-       { rgb={161,165,162}},
-     {rgb={109,110,108}},
+      { rgb = { 13, 105, 171 } },
+      { rgb = { 245, 205, 47 } },
+      { rgb = { 40, 127, 70 } },
+      { rgb = { 242, 243, 242 } },
+      { rgb = { 161, 165, 162 } },
+      { rgb = { 109, 110, 108 } },
       -- {name="black", rgb={27,42,52}},
    }
 }
 local fabuland = {
-   name='fabuland',
-   colors={
-      {rgb={255, 128, 20}},
-      {rgb={120,252,120}},
-      { rgb={215,197,153}},
-      { rgb={204,142,104}},
-      { rgb={137,181,196}},
-      { rgb={183,215,213}},
-      { rgb={203,132,60}},
-      { rgb={207, 138, 71}},
-      { rgb={242, 112, 94}},
-      { rgb={98, 71, 50}},
+   name = 'fabuland',
+   colors = {
+      { rgb = { 255, 128, 20 } },
+      { rgb = { 120, 252, 120 } },
+      { rgb = { 215, 197, 153 } },
+      { rgb = { 204, 142, 104 } },
+      { rgb = { 137, 181, 196 } },
+      { rgb = { 183, 215, 213 } },
+      { rgb = { 203, 132, 60 } },
+      { rgb = { 207, 138, 71 } },
+      { rgb = { 242, 112, 94 } },
+      { rgb = { 98, 71, 50 } },
    }
 }
 
 
 
 local jamesGulliverHancock = {
-   'd44553','e36f73','d2797b','cd8aa4','cc6b8f','df5a91','ec71a5','e595ab','dca9ac','ecbcc7','b5abac','a89ea1','996164','797073','796870','7f968c',
-   '39875e','2388a2','2599ab','205667','5b6b73','539693','5d9398','76a3ae','a4c6cd','aaaeaa','9fc84b','869048','676766','757066','937f64','947a60',
-   '9f7956','a98343','c0ab4e','af9d50','af9f5e','9e9c79','abaf85','a5a4a2','c1b6ac','cab39d','cdae9a','d9ad76','cfab60','cdbb6e','d9c054','dfc54b',
-   'e3ca57','ded869','d4d29d','c7c66f','bcc092','bdba94','bebcb2','b6af9b','baaf8d','b99b75','b99974','cb9370','bc7d5e','bd7151','c6735d','e17b55',
-   'e58f61','ed8b60','f0b364','f7d0b2','f2e3d9','dfdcd9','e6e5e2','ede8d5','e0dcbd','e9d22b','e1e1c0','d0cccc','c19490','995555','a19e9c','e5e3df',
-   'cb5f63','dfcaa7','ca3135','9a5e61','ad898a','454546',
+   'd44553', 'e36f73', 'd2797b', 'cd8aa4', 'cc6b8f', 'df5a91', 'ec71a5', 'e595ab', 'dca9ac', 'ecbcc7', 'b5abac', 'a89ea1',
+   '996164', '797073', '796870', '7f968c',
+   '39875e', '2388a2', '2599ab', '205667', '5b6b73', '539693', '5d9398', '76a3ae', 'a4c6cd', 'aaaeaa', '9fc84b', '869048',
+   '676766', '757066', '937f64', '947a60',
+   '9f7956', 'a98343', 'c0ab4e', 'af9d50', 'af9f5e', '9e9c79', 'abaf85', 'a5a4a2', 'c1b6ac', 'cab39d', 'cdae9a', 'd9ad76',
+   'cfab60', 'cdbb6e', 'd9c054', 'dfc54b',
+   'e3ca57', 'ded869', 'd4d29d', 'c7c66f', 'bcc092', 'bdba94', 'bebcb2', 'b6af9b', 'baaf8d', 'b99b75', 'b99974', 'cb9370',
+   'bc7d5e', 'bd7151', 'c6735d', 'e17b55',
+   'e58f61', 'ed8b60', 'f0b364', 'f7d0b2', 'f2e3d9', 'dfdcd9', 'e6e5e2', 'ede8d5', 'e0dcbd', 'e9d22b', 'e1e1c0', 'd0cccc',
+   'c19490', '995555', 'a19e9c', 'e5e3df',
+   'cb5f63', 'dfcaa7', 'ca3135', '9a5e61', 'ad898a', '454546',
 }
 
 local james = {
-   name="james",
+   name = "james",
    colors = {}
 }
 
 for i = 1, #jamesGulliverHancock do
-   local r,g,b = hex2rgb(jamesGulliverHancock[i])
-   table.insert(james.colors, { rgb={r,g,b}})
+   local r, g, b = hex2rgb(jamesGulliverHancock[i])
+   table.insert(james.colors, { rgb = { r, g, b } })
 end
 
 local picoColors = {
-   '000000','1D2B53','7E2553','008751','AB5236','5F574F','C2C3C7','FFF1E8',
-   'FF004D','FFA300','FFEC27','00E436','29ADFF','83769C','FF77A8','FFCCAA',
+   '000000', '1D2B53', '7E2553', '008751', 'AB5236', '5F574F', 'C2C3C7', 'FFF1E8',
+   'FF004D', 'FFA300', 'FFEC27', '00E436', '29ADFF', '83769C', 'FF77A8', 'FFCCAA',
 }
 
 local pico = {
-   name='pico',
+   name = 'pico',
    colors = {}
 }
 for i = 1, #picoColors do
-   local r,g,b = hex2rgb(picoColors[i])
-   table.insert(pico.colors, { rgb={r,g,b}})
+   local r, g, b = hex2rgb(picoColors[i])
+   table.insert(pico.colors, { rgb = { r, g, b } })
 end
 
 local childCraftColors = {
@@ -112,287 +117,287 @@ local childCraftColors = {
 
 
 local childCraft = {
-   name='childCraft',
+   name = 'childCraft',
    colors = {}
 }
 
 for i = 1, #childCraftColors do
-   local r,g,b = hex2rgb(childCraftColors[i])
-   table.insert(childCraft.colors, {rgb={r,g,b}})
+   local r, g, b = hex2rgb(childCraftColors[i])
+   table.insert(childCraft.colors, { rgb = { r, g, b } })
 end
 
 
 local gruvBoxColors = {
-'1D2021',
-'282828',
-'32302F',
-'3C3836',
-'504945',
-'665C54',
-'7C6F64',
-'7C6F64',
-'928374',
-'928374',
-'F9F5D7',
-'FBF1C7',
-'F2E5BC',
-'EBDBB2',
-'D5C4A1',
-'BDAE93',
-'A89984',
-'A89984',
-'FB4934',
-'B8BB26',
-'FABD2F',
-'83a598',
-'d3869b',
-'8ec07c',
-'FE8019',
-'CC241D',
-'98971A',
-'D79921',
-'458588',
-'B16286',
-'689d6a',
-'D65D0E',
-'9D0006',
-'79740E',
-'B57614',
-'076678',
-'8F3F71',
-'427B58',
-'AF3A03',
+   '1D2021',
+   '282828',
+   '32302F',
+   '3C3836',
+   '504945',
+   '665C54',
+   '7C6F64',
+   '7C6F64',
+   '928374',
+   '928374',
+   'F9F5D7',
+   'FBF1C7',
+   'F2E5BC',
+   'EBDBB2',
+   'D5C4A1',
+   'BDAE93',
+   'A89984',
+   'A89984',
+   'FB4934',
+   'B8BB26',
+   'FABD2F',
+   '83a598',
+   'd3869b',
+   '8ec07c',
+   'FE8019',
+   'CC241D',
+   '98971A',
+   'D79921',
+   '458588',
+   'B16286',
+   '689d6a',
+   'D65D0E',
+   '9D0006',
+   '79740E',
+   'B57614',
+   '076678',
+   '8F3F71',
+   '427B58',
+   'AF3A03',
 }
 
 local gruvBox = {
-   name='gruvBox',
+   name = 'gruvBox',
    colors = {}
 }
 
 for i = 1, #gruvBoxColors do
-   local r,g,b = hex2rgb(gruvBoxColors[i])
-   table.insert(gruvBox.colors, { rgb={r,g,b}})
+   local r, g, b = hex2rgb(gruvBoxColors[i])
+   table.insert(gruvBox.colors, { rgb = { r, g, b } })
 end
 
 local littleGreene = {
-   name='littleGreene',
-   colors= {}
+   name = 'littleGreene',
+   colors = {}
 }
 
 local littleGreeneColors = {
 
-    "FDFDFA",
-    "F8F8F8",
-    "EEEAE1",
-    "C8C5B7",
-    "989180",
-    "969183",
-    "8C8C8C",
-    "595A57",
-    "F9F7F2",
-    "F0F1EC",
-    "F4F3E7",
-    "EBEAE0",
-    "C9C5A8",
-    "C5BBA8",
-    "857E6C",
-    "5A4620",
-    "FCF6D8",
-    "F9F8E3",
-    "F8F4E3",
-    "F2ECD7",
-    "DFCEB1",
-    "CDB684",
-    "C09F66",
-    "AF732B",
-    "F4F2E5",
-    "F7EDE0",
-    "F3EEE8",
-    "DDC8AC",
-    "D4C59C",
-    "D3C3AE",
-    "BFB08F",
-    "92756F",
-    "F1E7DD",
-    "F3D5AF",
-    "E9DAC3",
-    "D2B6A0",
-    "C6B2A7",
-    "AE8A82",
-    "945E5C",
-    "5D3A40",
-    "EEF0F1",
-    "D4DACC",
-    "C2D2C0",
-    "A1A7A4",
-    "A4B4AF",
-    "627D85",
-    "525969",
-    "415968",
-    "E0E3C4",
-    "E1DBC6",
-    "B7B79F",
-    "A0A07D",
-    "A8AF96",
-    "8E9B8A",
-    "77815C",
-    "6D7D78",
-    "E5E4D0",
-    "DEEBDC",
-    "C6D6CA",
-    "A8BBA0",
-    "85A486",
-    "5C7C7E",
-    "446562",
-    "425958",
-    "D8BB5D",
-    "DFC774",
-    "CBC687",
-    "C9CB9A",
-    "A9C093",
-    "739153",
-    "5A6E48",
-    "33523A",
-    "D0A946",
-    "EED266",
-    "EDE068",
-    "C5C951",
-    "A8AF61",
-    "777F2D",
-    "566038",
-    "42431F",
-    "E1C022",
-    "FFE57B",
-    "F1F15C",
-    "74C84C",
-    "6AC59C",
-    "236D6F",
-    "03414E",
-    "063025",
-    "DCEDF1",
-    "BFD5CB",
-    "ADD4DE",
-    "5C8EA9",
-    "659AB2",
-    "558799",
-    "235580",
-    "003B59",
-    "B7CCDA",
-    "8597A3",
-    "8AA0AD",
-    "6C7892",
-    "5371B8",
-    "436382",
-    "29467B",
-    "12042A",
-    "DCC6BA",
-    "F5E8E6",
-    "ECD2CB",
-    "E9D0CF",
-    "C8A5A3",
-    "D5666C",
-    "CC3852",
-    "AE385D",
-    "BD2520",
-    "E3634B",
-    "F0841F",
-    "9F3C19",
-    "874535",
-    "6C2322",
-    "611117",
-    "711324",
-    "493B3C",
-    "2E2B14",
-    "132019",
-    "294356",
-    "2E3742",
-    "0C1B24",
-    "2E2D2E",
-    "040E0F"
+   "FDFDFA",
+   "F8F8F8",
+   "EEEAE1",
+   "C8C5B7",
+   "989180",
+   "969183",
+   "8C8C8C",
+   "595A57",
+   "F9F7F2",
+   "F0F1EC",
+   "F4F3E7",
+   "EBEAE0",
+   "C9C5A8",
+   "C5BBA8",
+   "857E6C",
+   "5A4620",
+   "FCF6D8",
+   "F9F8E3",
+   "F8F4E3",
+   "F2ECD7",
+   "DFCEB1",
+   "CDB684",
+   "C09F66",
+   "AF732B",
+   "F4F2E5",
+   "F7EDE0",
+   "F3EEE8",
+   "DDC8AC",
+   "D4C59C",
+   "D3C3AE",
+   "BFB08F",
+   "92756F",
+   "F1E7DD",
+   "F3D5AF",
+   "E9DAC3",
+   "D2B6A0",
+   "C6B2A7",
+   "AE8A82",
+   "945E5C",
+   "5D3A40",
+   "EEF0F1",
+   "D4DACC",
+   "C2D2C0",
+   "A1A7A4",
+   "A4B4AF",
+   "627D85",
+   "525969",
+   "415968",
+   "E0E3C4",
+   "E1DBC6",
+   "B7B79F",
+   "A0A07D",
+   "A8AF96",
+   "8E9B8A",
+   "77815C",
+   "6D7D78",
+   "E5E4D0",
+   "DEEBDC",
+   "C6D6CA",
+   "A8BBA0",
+   "85A486",
+   "5C7C7E",
+   "446562",
+   "425958",
+   "D8BB5D",
+   "DFC774",
+   "CBC687",
+   "C9CB9A",
+   "A9C093",
+   "739153",
+   "5A6E48",
+   "33523A",
+   "D0A946",
+   "EED266",
+   "EDE068",
+   "C5C951",
+   "A8AF61",
+   "777F2D",
+   "566038",
+   "42431F",
+   "E1C022",
+   "FFE57B",
+   "F1F15C",
+   "74C84C",
+   "6AC59C",
+   "236D6F",
+   "03414E",
+   "063025",
+   "DCEDF1",
+   "BFD5CB",
+   "ADD4DE",
+   "5C8EA9",
+   "659AB2",
+   "558799",
+   "235580",
+   "003B59",
+   "B7CCDA",
+   "8597A3",
+   "8AA0AD",
+   "6C7892",
+   "5371B8",
+   "436382",
+   "29467B",
+   "12042A",
+   "DCC6BA",
+   "F5E8E6",
+   "ECD2CB",
+   "E9D0CF",
+   "C8A5A3",
+   "D5666C",
+   "CC3852",
+   "AE385D",
+   "BD2520",
+   "E3634B",
+   "F0841F",
+   "9F3C19",
+   "874535",
+   "6C2322",
+   "611117",
+   "711324",
+   "493B3C",
+   "2E2B14",
+   "132019",
+   "294356",
+   "2E3742",
+   "0C1B24",
+   "2E2D2E",
+   "040E0F"
 }
 for i = 1, #littleGreeneColors do
-   local r,g,b = hex2rgb(littleGreeneColors[i])
-   table.insert(littleGreene.colors, { rgb={r,g,b}})
+   local r, g, b = hex2rgb(littleGreeneColors[i])
+   table.insert(littleGreene.colors, { rgb = { r, g, b } })
 end
 
 
 local quentinBlake = {
-   name='quentinBlake',
+   name = 'quentinBlake',
    colors = {}
 }
 
 local quentinBlakeColors = {
-'D9ccc5',
-'D1CCC0',
-'CCC4C9',
-'BDC0C9',
-'C77D52',
-'C7B6A9',
-'C4AC7C',
-'C2997A',
-'C2B7A3',
-'9E9691',
-'9C3E44',
-'9C9498',
-'9C5F43',
-'9C998E',
-'9C8D81',
-'997E45',
-'965D64',
-'96835A',
-'96755D',
-'8D8F94',
-'948F81',
-'8A918C',
-'798091',
-'768A7B',
-'4C5575',
-'4A7067',
-'6E4431',
-'6E6D5D',
-'6E615A',
-'6B5E42',
-'6B6A64',
-'6B6563',
-'626964',
-'56695B',
-'694F41',
-'545566',
-'613D41',
-'614E57',
-'5C5D61',
-'5E595C',
-'453B3D',
-'45362D',
-'424345',
-'454343',
-'384239',
-'423F42',
-'3E4240',
-'423A35',
-'42423E',
-'3E4035',
+   'D9ccc5',
+   'D1CCC0',
+   'CCC4C9',
+   'BDC0C9',
+   'C77D52',
+   'C7B6A9',
+   'C4AC7C',
+   'C2997A',
+   'C2B7A3',
+   '9E9691',
+   '9C3E44',
+   '9C9498',
+   '9C5F43',
+   '9C998E',
+   '9C8D81',
+   '997E45',
+   '965D64',
+   '96835A',
+   '96755D',
+   '8D8F94',
+   '948F81',
+   '8A918C',
+   '798091',
+   '768A7B',
+   '4C5575',
+   '4A7067',
+   '6E4431',
+   '6E6D5D',
+   '6E615A',
+   '6B5E42',
+   '6B6A64',
+   '6B6563',
+   '626964',
+   '56695B',
+   '694F41',
+   '545566',
+   '613D41',
+   '614E57',
+   '5C5D61',
+   '5E595C',
+   '453B3D',
+   '45362D',
+   '424345',
+   '454343',
+   '384239',
+   '423F42',
+   '3E4240',
+   '423A35',
+   '42423E',
+   '3E4035',
 
 }
 
 for i = 1, #quentinBlakeColors do
-   local r,g,b = hex2rgb(quentinBlakeColors[i])
-   table.insert(quentinBlake.colors, { rgb={r,g,b}})
+   local r, g, b = hex2rgb(quentinBlakeColors[i])
+   table.insert(quentinBlake.colors, { rgb = { r, g, b } })
 end
 
 -- this is returned
 function getAllPalettes()
    local palette = {
-      name='mix-and-match',
-      colors={}
-}
+      name = 'mix-and-match',
+      colors = {}
+   }
 
-   local palettes = {miffy, pico, lego, fabuland, james, childCraft, gruvBox, quentinBlake, littleGreene}
+   local palettes = { miffy, pico, lego, fabuland, james, childCraft, gruvBox, quentinBlake, littleGreene }
    for i = 1, #palettes do
       for j = 1, #palettes[i].colors do
-         table.insert(palette.colors,palettes[i].colors[j] )
+         table.insert(palette.colors, palettes[i].colors[j])
       end
    end
-   
+
    return palette
 end
