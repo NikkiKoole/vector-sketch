6264ccc4cc878b4920a84e94c81d5c945d20b62f
Nikki Koole <nikkikoole@gmail.com>
2022-10-04 15:14:50 +0200

    temp fix, just lod the resource from 2 locations

--- lib/basic-tools.lua
+++ lib/basic-tools.lua
@@ -33,23 +33,25 @@ function printC(c, ...)
    io.write(reset)
 end
 
-local base = '/Users/nikkikoole/Projects/vector-sketch'
+local base1 = '/Users/nikkikoole/Projects/love/vector-sketch'
+local base2 = '/Users/nikkikoole/Projects/vector-sketch'
 local function mountZip(filename, mountpoint)
-   --print(filename) 
+   --print(filename)
    local f = io.open(filename, 'r')
    if f then
       local filedata = love.filesystem.newFileData(f:read("*all"), filename)
       f:close()
       local result = love.filesystem.mount(filedata, mountpoint or 'zip')
       --print(inspect(result))
+      printC({ fg = 'black', bg = 'magenta' }, "loaded resources file :" .. filename)
       return result
    else
-      printC({fg = 'black', bg = 'red' }, "could not load resources file :"..filename)
+      printC({ fg = 'black', bg = 'red' }, "could not load resources file :" .. filename)
    end
 end
 
-mountZip(base .. '/resources.zip', '')
-
+mountZip(base1 .. '/resources.zip', '')
+mountZip(base2 .. '/resources.zip', '')
 -- you need require console. before the rpint overwrite below
 console = require 'vendor.console'
 
