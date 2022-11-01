26761d3ecc13ba1479634817ccff2b26ebd3af6b
Nikki Koole <nikki@floorplanner.com>
2022-11-01 10:09:46 +0100

    todo make some scenes do the ecs thing I think too

--- experiments/character-creation-kit/main.lua
+++ experiments/character-creation-kit/main.lua
@@ -1,3 +1,10 @@
+package.path = package.path .. ";../../?.lua"
+
+require 'lib.basic-tools'
+
+local camera = require 'lib.camera'
+local cam = require('lib.cameraBase').getInstance()
+
 
 function love.keypressed(key)
    if key == "escape" then love.event.quit() end
@@ -6,8 +13,14 @@ end
 function love.load()
    love.window.setMode(1024, 768, { resizable = true, vsync = true, minwidth = 400, minheight = 300, msaa = 2, highdpi = true })
    love.window.setTitle('☺♥ character creation kit ♥☺')
- 
 
 end
 
+
+
    
+function love.resize(w, h)
+   camera.setCameraViewport(cam, 1000, 1000)
+   cam:update(w, h)
+end
+
