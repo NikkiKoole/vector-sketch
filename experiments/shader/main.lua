215bbcd51bb43888684ec24ae0928db574b84d78
Nikki Koole <nikki@floorplanner.com>
2023-02-16 16:01:48 +0100

    ooh nice shader

--- experiments/shader/main.lua
+++ experiments/shader/main.lua
@@ -1,19 +1,18 @@
-
+local inspect = require 'inspect'
 --https://love2d.org/forums/viewtopic.php?t=88854
 local maskShader = love.graphics.newShader([[
 	uniform Image fill;
     uniform vec3 backgroundColor;
+    uniform mat2 uvTransform;
 
 	vec4 effect(vec4 color, Image mask, vec2 uv, vec2 fc) {
-        //return vec4(Texel(img, uv).aaa, 0.5 );
+        vec2 transformedUV = uv * uvTransform;
 
+        
 
-        vec3 patternMix = mix(backgroundColor.rgb, color.rgb, Texel(fill, uv).a * color.a);
+        vec3 patternMix = mix(backgroundColor.rgb, color.rgb, Texel(fill, transformedUV).a * color.a);
 
         return vec4(patternMix, Texel(mask, uv).r);
-
-
-		//return color * mix(Texel(imageB, uv), Texel(bg, uv), Texel(img, uv));
 	}
 ]])
 
@@ -24,33 +23,46 @@ end
 function love.load()
     line = love.graphics.newImage('outlineW.png')
     texture = love.graphics.newImage('textureW.png')
+    texture:setWrap('mirroredrepeat', 'mirroredrepeat')
     texture7 = love.graphics.newImage('texture7.png')
     mask = love.graphics.newImage('mask.png')
     maskBW = love.graphics.newImage('maskBW.png')
     maskBWA = love.graphics.newImage('maskBWA.png')
+
+    delta = 0
+end
+
+function love.update(dt)
+    delta = delta + dt
 end
 
 
 function love.draw()
+
+    local w,h = texture:getDimensions()
     love.graphics.clear(0,1,0)
     love.graphics.setColor(1,1,1)
-   -- if mask then
-       -- love.graphics.setBlendMode('alpha', 'premultiplied')
         
          love.graphics.setShader(maskShader)
-       -- love.graphics.setColor(color1[1], color1[2], color1[3], alpha1/5)
+
+         local transform = love.math.newTransform( )
+         local s = love.math.random()*12
+         transform:rotate((delta / 10) % (math.pi*2))
+         transform:scale(1,1)
+         
+       --  transform:translate(w/2, h/2)
+           
+            local m1,m2,_,_,m5,m6 = transform:getMatrix()
+
          maskShader:send('fill', texture)
-         maskShader:send('backgroundColor', {0,0,1})
-      --  love.graphics.setColor(color2[1], color2[2], color2[3], alpha2/5 )
-        -- maskShader:send('imageB', mask)
-        
-        
-        love.graphics.setColor(1,0,0,0.2)
-         love.graphics.draw(maskBWA)
+         maskShader:send('backgroundColor', {0,0,1, 1})
+         maskShader:send('uvTransform', {{m1,m2}, {m5,m6}})
+  
+        love.graphics.setColor(1,0,0,1)
+        love.graphics.draw(maskBWA)
         love.graphics.setShader()
-        love.graphics.setBlendMode("alpha") ---<<<<
-    -- end
 
+        love.graphics.setColor(1,0,0)
+        love.graphics.draw(line)
 
-   -- love.graphics.draw(line)
 end
