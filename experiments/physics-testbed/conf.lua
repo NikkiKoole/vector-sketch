6297a92fffb9b81ad0796abfcca3cf765ca785da
Nikki Koole <nikkikoole@gmail.com>
2024-01-18 06:12:40 +0100

    asdasdf

---
+++ experiments/physics-testbed/conf.lua
@@ -0,0 +1,37 @@
+local debug = true
+
+
+function love.conf(t)
+    -- 2796 / 2, 1290 / 2,
+    -- t.window.width = 2796 / 2
+    --- t.window.height = 1290 / 2
+    --t.window.width = 2688 / 2
+    --t.window.height = 1242 / 2
+    --t.window.width = 2208 / 2
+    --t.window.height = 1242 / 2
+
+    t.window.width = 1024
+    t.window.height = 768
+
+    t.window.title = "Testbed"
+    --   t.window.icon = "icoon.png"
+
+    -- t.window.resizable = true
+    --t.window.msaa = 4
+    t.window.highdpi = false
+    t.window.vsync = 1
+    -- t.window.msaa = 2
+    -- t.window.highdpi = true
+    -- t.window.vsync = true
+    if debug then
+        --t.window.resizable = true
+        t.window.resizable = true
+        t.window.borderless = false
+        --t.window.minwidth = 1024 / 2
+        --t.window.minheight = 768 / 2
+    else
+        t.window.borderless = true -- setting this will hide the status bar
+        t.window.fullscreen = true
+        t.window.resizable = false
+    end
+end
--- experiments/physics-testbed/main.lua
+++ experiments/physics-testbed/main.lua
@@ -6,43 +6,87 @@ local phys    = require 'lib.mainPhysics'
 
 local numbers = require 'lib.numbers'
 
-function makeChainGround()
-    local width, height = love.graphics.getDimensions()
-    local points = {}
 
-    for i = -1500, 1500 do
-        local cool = 4.78
-        local amplitude = 50 * cool
-        local frequency = 30 + love.math.random() * 3
-        local h = love.math.noise(i / frequency, 1, 1) * amplitude
-        local y1 = h - (amplitude / 2)
-
-        local cool = 1.78
-        local amplitude = 200 * cool
-        local frequency = 17
-        local h = love.math.noise(i / frequency, 1, 1) * amplitude
-        local y2 = h - (amplitude / 2)
-
-        table.insert(points, i * 100)
-
-        -- how to get this to be a hill slope ?
-        -- well, i just want -1000 to be at some height and 1000 to rets is linear map
-
-
-        local linear = numbers.mapInto(i, -1500, 1500, -25000, 25000)
-
-        table.insert(points, y1 + y2 + linear)
-    end
-
-    print(#points)
-
+function initGround()
     local thing = {}
     thing.body = love.physics.newBody(world, 0, 0)
-    thing.shape = love.physics.newChainShape(false, points)
-    thing.fixture = love.physics.newFixture(thing.body, thing.shape)
-    thing.fixture:setUserData("border")
-    thing.fixture:setFriction(1)
+    updateGround(thing)
     return thing
+end
+
+function updateGround(ground)
+    local w, h = love.graphics.getDimensions()
+    -- camera.setCameraViewport(cam, w, h)
+    -- camera.centerCameraOnPosition(w / 2, h / 2 - 1000, 3000, 3000)
+    local camtlx, camtly = cam:getWorldCoordinates(0, 0)
+    local cambrx, cambry = cam:getWorldCoordinates(w, h)
+    local boxWorldWidth = cambrx - camtlx
+    local boxWorldHeight = cambry - camtly
+
+    -- get the correct steps to calulate values at
+    -- think about the end parts..
+    --print(camtlx, cambrx)
+    local stepSize = 100
+    local steps = math.ceil(boxWorldWidth / stepSize)
+    --print(math.ceil(steps))
+
+    local points = {}
+
+    if ground.fixture then
+        ground.fixture:destroy()
+    end
+
+    local extraSteps = 100
+    for i = 1 - extraSteps, steps + 2 + extraSteps do
+        local index = math.floor(camtlx / stepSize) + (i - 1)
+        --print(i, index)
+
+        -- smooth waves
+        local cool = 10.78
+        local amplitude = 150 * cool
+        --local f2 = 30
+        local frequency = 30
+        local h = love.math.noise(index / frequency, 1, 1) * amplitude
+        local y1 = h - (amplitude / 2)
+
+
+        --uphills
+        local cool = 10.78
+        local amplitude = 170 * cool
+        local frequency = 17
+        local h = love.math.noise(index / frequency, 1, 1) * amplitude
+        local y2 = h - (amplitude / 2)
+
+
+
+        -- the roughness
+
+
+
+        local cool = 10.78
+        local amplitude = 20 * cool
+        local frequency = 3
+        local h = love.math.noise(index / frequency, 1, 1) * amplitude
+        local y3 = h - (amplitude / 2)
+
+        -- sometimes i want roughness, sometimes i odnt
+        local r = ((math.sin(index / 30) + 1) / 2)
+        y3 = y3 * r
+
+
+        -- the downhiill steepness
+        local linear = numbers.mapInto(index, -20 * stepSize, 20 * stepSize, -500 * stepSize, 500 * stepSize)
+        local x = (math.floor(camtlx / stepSize) * stepSize) + (i - 1) * stepSize
+
+        table.insert(points, x)
+        table.insert(points, y1 + y2 + y3 + linear)
+    end
+
+    ground.shape = love.physics.newChainShape(false, points)
+    ground.fixture = love.physics.newFixture(ground.body, ground.shape)
+    ground.fixture:setUserData("ground")
+    ground.fixture:setFriction(1)
+    --print(boxWorldWidth, boxWorldHeight)
 end
 
 function makeBall(x, y, radius)
@@ -61,30 +105,49 @@ function startExample(number)
     --  local width, height = love.graphics.getDimensions()
     --  love.physics.setMeter(500)
     --  world = love.physics.newWorld(0, 9.81 * love.physics.getMeter(), true)
-    ground = makeChainGround()
-    makeBall(0 + 1000, -200, 1200)
-    makeBall(0 + 1000, -200, 200)
-    makeBall(0, -100, 50)
+    ground = initGround()
+    ball = makeBall(0, -500, 100)
+    -- makeBall(0 + 1000, -200, 200)
+    -- makeBall(0, -100, 50)
     -- world:setCallbacks(beginContact, endContact, preSolve, postSolve)
 end
 
 function love.load()
     startExample()
     local w, h = love.graphics.getDimensions()
-
+    -- ground = initGround()
     camera.setCameraViewport(cam, w, h)
-    camera.centerCameraOnPosition(w / 2, h / 2 - 1000, 3000, 3000)
+    camera.centerCameraOnPosition(0, 0, 3000, 3000)
 end
 
 function love.update(dt)
+    updateGround(ground)
     world:update(dt)
     phys.handleUpdate(dt, cam)
+
+
+    -- center camera on wheel
+    -- do this stuff
+    -- https://www.gamedeveloper.com/design/camera-logic-in-a-2d-platformer
+    local worldX, worldY = ball.body:getWorldPoint(0, 0)
+
+
+    local velX, velY = ball.body:getLinearVelocity()
+    local viewWidth = numbers.mapInto(math.abs(velX), 0, 10000, 3000, 4000)
+    camera.centerCameraOnPosition(worldX, worldY, viewWidth, viewWidth)
+    --    print(velX)
+
+    -- print(worldX, worldY)
 end
 
 function love.draw()
     cam:push()
     phys.drawWorld(world)
     cam:pop()
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.print("Current FPS: " .. tostring(love.timer.getFPS()), 10, 10)
+    local velX, velY = ball.body:getLinearVelocity()
+    love.graphics.print('ball speed: ' .. math.floor(velX), 10, 30)
 end
 
 function love.keypressed(k)
@@ -112,7 +175,9 @@ local function pointerPressed(x, y, id)
 end
 
 function love.mousepressed(x, y, button)
-    pointerPressed(x, y, 'mouse')
+    if button == 1 then
+        pointerPressed(x, y, 'mouse')
+    end
 end
 
 local function pointerReleased(x, y, id)
