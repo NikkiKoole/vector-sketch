acc84de4c1c62f9df12f9e6be9913d30fb55ef3f
Nikki Koole <nikkikoole@gmail.com>
2025-08-23 09:13:26 +0200

    humpie

--- experiments/playtime/main.lua
+++ experiments/playtime/main.lua
@@ -132,11 +132,11 @@ function love.load(args)
 
     -- sceneLoader.loadScene(cwd .. '/scripts/limits.playtime.json')
     --sceneLoader.loadScene(cwd .. '/scripts/limitsagain.playtime.json')
+    humanoidInstance = CharacterManager.createCharacter("humanoid", 100, 300, .1)
+    --  humanoidInstance = CharacterManager.createCharacter("humanoid", 300, 300, .3)
 
-    humanoidInstance = CharacterManager.createCharacter("humanoid", 300, 300, .3)
 
-
-    humanoidInstance = CharacterManager.createCharacter("humanoid", 800, 300, 1)
+    --humanoidInstance = CharacterManager.createCharacter("humanoid", 800, 300, 1)
 
 
     --  humanoidInstance = CharacterManager.createCharacter("humanoid", 300, 800, .5)
